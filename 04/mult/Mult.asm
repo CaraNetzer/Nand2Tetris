a468bb7663e0cd6e630ba7909b3bf1b30efde250
CaraNetzer <cara.netzer@gmail.com>
2023-11-06 14:44:40 -0500

    mult first try

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -3,10 +3,47 @@
 // by Nisan and Schocken, MIT Press.
 // File name: projects/04/Mult.asm
 
+//CNETZER - project 04
+
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 //
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+//psuedo code:
+//set i = 0
+//set sum = 0
+//(loop)
+//if i - R1 = 0
+  //break
+//else  
+  //sum = sum + R0
+//loop
+//set R2 = sum
+
+  @i
+  M=0
+  @sum
+  M=0
+(LOOP)
+  @i
+  D=M 
+  @R1
+  D=D-A //i - R1, we want to add R0 to itself, R1 times
+  @END //set A reg to END
+  D;JEQ //jump to END
+  @R0
+  D=M
+  @sum
+  M=D+M
+  D=M
+  @R2
+  M=D //store the current total in R2
+  @i
+  M=M+1  
+  @LOOP
+  0;JMP
+(END)
+  @END
+  0;JMP
