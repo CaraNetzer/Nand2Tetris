510fcfa01ca297ad2893d1c4e3a44b88f67fd306
CaraNetzer <cara.netzer@gmail.com>
2023-11-06 16:02:26 -0500

    fill partially done

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -3,6 +3,8 @@
 // by Nisan and Schocken, MIT Press.
 // File name: projects/04/Fill.asm
 
+//CNETZER - project 04
+
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
@@ -11,4 +13,28 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// psuedo code:
+//(loop)
+//set var to KEYBOARD
+//if keyboard <> 0, 
+  //loop through screen and set all the screen registers to 1?
+//else
+  //set all screen registers to 0
+//loop
+
+(LOOP)
+  @R0
+  D=M //R0 should be 0
+  @KBD
+  D=D-M //if keyboard is pressed, M!=0, so D will be not be 0
+  @WHITE
+  D;JEQ //jump to top of loop if keyboard is not pressed --> D = 0 - 0 = 0
+  @SCREEN
+  M=-1
+  @LOOP
+  0;JMP
+(WHITE)
+  @SCREEN
+  M=0
+  @LOOP
+  0;JMP
