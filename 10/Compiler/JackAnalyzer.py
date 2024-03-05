2bd42083bfc623a85b3b494cda8e19d94a667dee
CaraNetzer <cara.netzer@gmail.com>
2024-03-04 22:35:53 -0500

    tokenizer comparison successful

--- 10/Compiler/JackAnalyzer.py
+++ 10/Compiler/JackAnalyzer.py
@@ -8,7 +8,7 @@ def main():
     in_file_path = sys.argv[1:][0]
 
     if os.path.isdir(in_file_path):
-        files = os.listdir(in_file_path)      
+        files = os.listdir(in_file_path)
 
         for file_name in files:
             if file_name.endswith(".jack"):
@@ -20,31 +20,55 @@ def main():
 
 
 def process_file(in_file_name, in_dirname):
-    out_file_name = in_file_name[0: -4] + "xml"
+    out_file_name = in_file_name[0: -4] + "test.xml"
     out_file_path = os.path.join(in_dirname, out_file_name)
 
     tokenizer = JackTokenizer.JackTokenizer(os.path.join(in_dirname, in_file_name), out_file_path)
     execute(tokenizer)
 
+def append_tokens(words, token_list):
+    for word in words:
+        tokens_and_words = re.split("(\W)", word)
+        # print("token_words: " + " ".join(token_words))
+        for token in tokens_and_words:
+            if token != '':
+                token_list.append(token)
+
 
 def execute(tokenizer):
     tokens = []
     current_token = ""
-    
+
     for line in tokenizer.in_file:
         if not line.startswith("//"):
-            line = re.split("//", line)[0].strip()
-            for character in line:
-                if not re.match("[\s+|\W]", character):
-                    current_token += character
-                elif re.match("[\W]", character):
-                    current_token += character
-                    tokens.append(current_token)
-                    current_token = ""
-                else:
-                    tokens.append(current_token)
-                    current_token = ""
-                
-    print(tokens)
+            line = re.split("(/\*)|(//)", line)[0].strip()
+            if line.__contains__("\""):
+                words = line.split("\"")
+                append_tokens(words[0].split(" "), tokens)
+                tokens.append(words[1])
+                append_tokens(words[2].split(" "), tokens)
+            else:
+                words = line.split(" ")
+                append_tokens(words, tokens)
+
+    tokenizer.out_file.write("<tokens>\n")
+
+    print(tokenizer.out_file)
+
+    for token in tokens:
+        tokenizer.current_token = token
+
+        if tokenizer.token_type() == "KEYWORD":
+            tokenizer.out_file.write(f"<keyword> {tokenizer.current_token} </keyword>\n")
+        if tokenizer.token_type() == "SYMBOL":
+            tokenizer.out_file.write(f"<symbol> {tokenizer.symbol()} </symbol>\n")
+        if tokenizer.token_type() == "IDENTIFIER":
+            tokenizer.out_file.write(f"<identifier> {tokenizer.current_token} </identifier>\n")
+        if tokenizer.token_type() == "INT_CONST":
+            tokenizer.out_file.write(f"<integerConstant> {tokenizer.current_token} </integerConstant>\n")
+        if tokenizer.token_type() == "STRING_CONST":
+            tokenizer.out_file.write(f"<stringConstant> {tokenizer.current_token} </stringConstant>\n")
+
+    tokenizer.out_file.write("</tokens>\n")
 
 main()
--- 10/Compiler/JackTokenizer.py
+++ 10/Compiler/JackTokenizer.py
@@ -1,8 +1,41 @@
+import re
+
 class JackTokenizer:
+    current_token = ""
+
+    keywords = [
+        "class", "constructor", "function", "method", "field", "static", "var", "int",
+        "char", "boolean", "void", "true", "false", "null", "this", "let", "do", "if", "else",
+        "while", "return"
+    ]
+
+    symbols = [
+        '{', '}', '(', ')', '[', ']', '.', ',', ';', '+', '-', '*', '/', '&', '|', '<', '>', '=', '_'
+    ]
 
     def __init__(self, in_file_path, out_file_path):
         self.in_file = open(in_file_path, "rt")
         self.out_file = open(out_file_path, "at", buffering = 1024)
 
-    def hasMoreTokens(self):
-        return self.in_file.read()
+
+    def token_type(self):
+        if self.current_token in self.keywords:
+            return "KEYWORD"
+        elif self.current_token in self.symbols:
+            return "SYMBOL"
+        elif re.match("\d", self.current_token):
+            return "INT_CONST"
+        elif self.current_token.__contains__(" "):
+            return "STRING_CONST"
+        else:
+            return "IDENTIFIER"
+
+    def symbol(self):
+        if self.current_token == '<':
+            return "&lt;"
+        elif self.current_token == '>':
+            return "&gt;"
+        elif self.current_token == '&':
+            return "&amp;"
+        else:
+            return self.current_token
