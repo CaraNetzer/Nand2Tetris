9866d2fccdd5dda3eff1dd8643e6fbf4191f2fe3
cnetzer <cnetzer@DEV-C2MPWG3>
2024-03-05 15:59:15 -0500

    moved all tokenizing logic to tokenizer module

--- 10/Compiler/JackAnalyzer.py
+++ 10/Compiler/JackAnalyzer.py
@@ -24,62 +24,11 @@ def process_file(in_file_name, in_dirname):
     out_file_path = os.path.join(in_dirname, out_file_name)
 
     tokenizer = JackTokenizer.JackTokenizer(os.path.join(in_dirname, in_file_name), out_file_path)
-    execute(tokenizer)
-
-def append_tokens(words, token_list):
-    for word in words:
-        tokens_and_words = re.split("(\W)", word)
-        # print("token_words: " + " ".join(token_words))
-        for token in tokens_and_words:
-            if token != '':
-                token_list.append(token)
+    tokenizer.execute()
+    compilationEngine = CompilationEngine.CompilationEngine(tokenizer, out_file_path)
+    compilationEngine.compileClass()
 
 
-def execute(tokenizer):
-    tokens = []
-    current_token = ""
-    block_comment = False
 
-    for line in tokenizer.in_file:
-        if block_comment:
-            if line.__contains__("*/"):
-                block_comment = False
-            continue
-        if line.strip().startswith("/*"):
-            block_comment = True
-            if line.__contains__("*/"):
-                block_comment = False
-            continue
-
-        if not line.startswith("//"):
-            line = re.split("(/\*)|(//)", line)[0].strip()
-            if line.__contains__("\""):
-                words = line.split("\"")
-                append_tokens(words[0].split(" "), tokens)
-                tokens.append(words[1])
-                append_tokens(words[2].split(" "), tokens)
-            else:
-                words = line.split(" ")
-                append_tokens(words, tokens)
-
-    tokenizer.out_file.write("<tokens>\n")
-
-    print(tokenizer.out_file)
-
-    for token in tokens:
-        tokenizer.current_token = token
-
-        if tokenizer.token_type() == "KEYWORD":
-            tokenizer.out_file.write(f"<keyword> {tokenizer.current_token} </keyword>\n")
-        if tokenizer.token_type() == "SYMBOL":
-            tokenizer.out_file.write(f"<symbol> {tokenizer.symbol()} </symbol>\n")
-        if tokenizer.token_type() == "IDENTIFIER":
-            tokenizer.out_file.write(f"<identifier> {tokenizer.current_token} </identifier>\n")
-        if tokenizer.token_type() == "INT_CONST":
-            tokenizer.out_file.write(f"<integerConstant> {tokenizer.current_token} </integerConstant>\n")
-        if tokenizer.token_type() == "STRING_CONST":
-            tokenizer.out_file.write(f"<stringConstant> {tokenizer.current_token} </stringConstant>\n")
-
-    tokenizer.out_file.write("</tokens>\n")
-
+            
 main()
--- 10/Compiler/JackTokenizer.py
+++ 10/Compiler/JackTokenizer.py
@@ -2,6 +2,7 @@ import re
 
 class JackTokenizer:
     current_token = ""
+    tagged_tokens = []
 
     keywords = [
         "class", "constructor", "function", "method", "field", "static", "var", "int",
@@ -17,6 +18,11 @@ class JackTokenizer:
         self.in_file = open(in_file_path, "rt")
         self.out_file = open(out_file_path, "at", buffering = 1024)
 
+    
+    def advance(self):
+        current_index = self.tagged_tokens.index(self.current_token)
+        self.current_token = self.tagged_tokens[current_index + 1]
+
 
     def token_type(self):
         if self.current_token in self.keywords:
@@ -30,7 +36,7 @@ class JackTokenizer:
         else:
             return "IDENTIFIER"
 
-    def symbol(self):
+    def convert_symbol(self):
         if self.current_token == '<':
             return "&lt;"
         elif self.current_token == '>':
@@ -39,3 +45,67 @@ class JackTokenizer:
             return "&amp;"
         else:
             return self.current_token
+
+
+    def keyWord(self):
+        keyword = self.current_token.split(" ")[1]
+        print(self.keywords.__getitem__(self.keywords.index(keyword)).upper())
+        return self.keywords.__getitem__(self.keywords.index(keyword)).upper()
+    
+    
+    def symbol(self):
+        symbol = self.current_token.split(" ")[1]
+        print(self.symbols.__getitem__(self.symbols.index(symbol)).upper())
+        return self.symbols.__getitem__(self.symbols.index(symbol)).upper()
+        
+    
+    def append_tokens(words, token_list):
+        for word in words:
+            tokens_and_words = re.split("(\W)", word)
+            for token in tokens_and_words:
+                if token != '':
+                    token_list.append(token)
+
+
+    def execute(self):
+        tokens = []
+        block_comment = False
+
+        for line in self.in_file:
+            if block_comment:
+                if line.__contains__("*/"):
+                    block_comment = False
+                continue
+            if line.strip().startswith("/*"):
+                block_comment = True
+                if line.__contains__("*/"):
+                    block_comment = False
+                continue
+
+            if not line.startswith("//"):
+                line = re.split("(/\*)|(//)", line)[0].strip()
+                if line.__contains__("\""):
+                    words = line.split("\"")
+                    self.append_tokens(words[0].split(" "), tokens)
+                    tokens.append(words[1])
+                    self.append_tokens(words[2].split(" "), tokens)
+                else:
+                    words = line.split(" ")
+                    self.append_tokens(words, tokens)
+
+
+        for token in tokens:
+            self.current_token = token
+
+            if self.token_type() == "KEYWORD":
+                self.tagged_tokens.append(f"<keyword> {self.current_token} </keyword>")
+            if self.token_type() == "SYMBOL":
+                self.tagged_tokens.append(f"<symbol> {self.convert_symbol()} </symbol>")
+            if self.token_type() == "IDENTIFIER":
+                self.tagged_tokens.append(f"<identifier> {self.current_token} </identifier>")
+            if self.token_type() == "INT_CONST":
+                self.tagged_tokens.append(f"<integerConstant> {self.current_token} </integerConstant>")
+            if self.token_type() == "STRING_CONST":
+                self.tagged_tokens.append(f"<stringConstant> {self.current_token} </stringConstant>")
+
+            # self.out_file.write(f"<stringConstant> {self.current_token} </stringConstant>\n")
