44f14a1fb07a6fa626b18d1ab292dd2ce1be9c55
CaraNetzer <cara.netzer@gmail.com>
2024-05-10 22:48:43 -0400

    tokenizing string constants done

--- 11/Compiler/JackTokenizer.c
+++ 11/Compiler/JackTokenizer.c
@@ -1,4 +1,5 @@
 #include <ctype.h>
+#include <assert.h>
 #include <libgen.h>
 #include <stdio.h>
 #include <stdbool.h>
@@ -41,7 +42,7 @@ void append_token(char *token) {
 
     if(tokenizer->next_index > tokenizer->max_tokens) {
         char **new_tokens = reallocarray(tokenizer->tokens, tokenizer->max_tokens * 2, sizeof(char*));
-    
+
         if(new_tokens) {
             tokenizer->tokens = new_tokens;
             tokenizer->max_tokens *= 2;
@@ -60,7 +61,7 @@ void append_tokenized_token(token *tok, int i) {
 
     if(i + 1 > tokenizer->max_tokens) {
         token **new_tokenized_tokens = reallocarray(tokenizer->tokens, tokenizer->max_tokens * 2, sizeof(token));
-    
+
         if(new_tokenized_tokens) {
             tokenizer->tokenized_tokens = new_tokenized_tokens;
             tokenizer->max_tokens *= 2;
@@ -71,9 +72,118 @@ void append_tokenized_token(token *tok, int i) {
     }
 }
 
+char* process_string_constant(char* line_position) {
+
+    char *string_token = malloc(BUFSIZ);
+    printf("line with string: %s\n", line_position);
+
+    /* while (strncmp(line_position, "\"", 1)) { */
+    /*   line_position += 1; */
+    /* } */
+
+    if (!strncmp(line_position, "\"", 1)) {
+      printf("string token: '%s'\n", string_token);
+      strncat(string_token, line_position, 1);
+      line_position += 1;
+
+      while (strncmp(line_position, "\"", 1)) {
+        strncat(string_token, line_position, 1);
+        line_position += 1;
+        printf("string token: '%s'\n", string_token);
+      }
+
+      if (!strncmp(line_position, "\"", 1)) {
+        strncat(string_token, line_position, 1);
+        line_position += 1;
+        printf("string token: '%s'\n", string_token);
+      }
+
+      printf("string token: '%s'\n", string_token);
+      append_token(string_token);
+    }
+
+    printf("after append string_token : %s, line position: %s\n", string_token, line_position);
+
+    return line_position;
+}
+
+
+void loop_through_words(char* line_position) {
+
+  assert(line_position);
+  /* int word_count = 1; */
+  bool string_appended;
+
+  char *line = strdup(line_position);
+  printf("line: %s\n", line);
+
+  // region: words = line.split(" ")
+  char *token = strtok(line_position, " ");
+
+  while (token != NULL) {
+    string_appended = false;
+    char *sub_token = strdup(token);
+    char string_sub_token[BUFSIZ];
+
+      int token_length = strlen(sub_token);
+      for(int i = 0; i < token_length + 1; i++) {
+
+        printf("sub_token beginning of for loop: '%s', token[%d]: %c\n", sub_token, i, token[i]);
+
+        if(!isspace(token[i])) {
+
+          if(!strncmp(sub_token, "\"", 1)) { //tokenizes string including double quotation marks
+            /* printf("sub_token with \": %s\n", sub_token); */
+            /* printf("line: %s\n", line); */
+            /* printf("%p, %p, %p, %p, %p\n", line, token, line_position, line + (token - line_position), line + (token - line_position) + i); */
+            char *position_before, *position_after;
+            int delta;
+            position_before = line + (token - line_position) + i;
+            position_after = process_string_constant(position_before);
+            delta = position_after - position_before;
+            i += delta;
+            printf("token + i: %s\n", token + i);
+            token = strtok(token + i, " ");
+            string_appended = true;
+          }
+          else if (!isalnum(token[i])) {
+            printf("sub_token alnum? '%s'\n", &sub_token[i]);
+
+            if(0 != strcmp(string_sub_token, "")) {
+              append_token(string_sub_token);
+              printf("string_sub_token being appended: %.50s\n", string_sub_token);
+            }
+
+            string_sub_token[0] = '\0';
+
+            if(0 != strcmp(sub_token, "") && 0 != strcmp(sub_token, "\n")) {
+              append_token(strndup(sub_token, 1));
+              printf("sub_token appended: '%.1s'\n", sub_token);
+            }
+            sub_token += 1;
+          }
+
+          else {
+            strncat(string_sub_token, sub_token, 1);
+            printf("string_sub_token after strncat: %.50s\n", string_sub_token);
+            sub_token += 1;
+          }
+        }
+      }
+
+      /* printf("word count: %d\n", word_count); */
+      /* for (int i = 0; i < word_count; i++) { */
+      if(!string_appended) {
+        token = strtok(NULL, " "); // need to pass NULL to strtok after first call
+      }
+      printf("token after strtok: %.50s\n", token);
+      /* } */
+  }
+  // #endregion
+}
 
 void tokenizer_execute(jack_tokenizer* tokenizer) {
-    
+
     bool block_comment = false;
 
     char line[BUFSIZ];
@@ -102,7 +212,7 @@ void tokenizer_execute(jack_tokenizer* tokenizer) {
             }
             continue;
         }
-        
+
         if(strncmp("//", line_position, 2) != 0) { // skip lines that are single line comments
             if(strstr(line_position, "/*") != NULL) {
                 char* beginning_of_comment = strstr(line_position, "/*");
@@ -113,54 +223,9 @@ void tokenizer_execute(jack_tokenizer* tokenizer) {
                 *beginning_of_comment = '\0';
             }
 
+            // create tokenizer->tokens list
+            loop_through_words(line_position);
 
-            if(strstr(line_position, "\"") != NULL) { //tokenizes string including parentheses
-                char *string_token = malloc(BUFSIZ);
-                // printf("%s", line_position);
-                strncat(string_token, line_position, 1);
-                while (!strcmp(line_position, "\"")) {
-                    strncat(string_token, line_position, 1);
-                    line_position += 1;
-                }
-                strncat(string_token, line_position, 1);
-                append_token(string_token);
-
-            } else {
-
-                // region: words = line.split(" ")
-                char *token = strtok(line_position, " ");
-
-                while (token != NULL) {
-                    char *sub_token = strdup(token);
-                    char string_sub_token[BUFSIZ];
-
-                    int token_length = strlen(sub_token);
-                    for(int i = 0; i < token_length + 1; i++) {
-
-                        if(!isspace(token[i])) {
-
-                            if (!isalnum(token[i])) {
-                                if(0 != strcmp(string_sub_token, "")) {
-                                    append_token(string_sub_token);
-                                }
-
-                                string_sub_token[0] = '\0';
-
-                                if(0 != strcmp(sub_token, "")) {
-                                    append_token(strndup(sub_token, 1));
-                                }
-                            } else {
-                                strncat(string_sub_token, sub_token, 1);
-                            }
-                        }
-                        sub_token += 1;
-                    }
-
-                    token = strtok(NULL, " "); // need to pass NULL to strtok after first call
-
-                }
-                // #endregion
-            }
         }
     }
 
